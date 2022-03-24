3e99bad6a29e604abb7c36649b300e395554379a
wenleeqc <74733659+wenleeqc@users.noreply.github.com>
2022-03-24 10:32:53 -0400

    updated index-script.js file

--- index-script.js
+++ index-script.js
@@ -1,10 +1,10 @@
 const quote = document.querySelector('.quote');
-const quotes = document.querySelectorAll('.quotes');
+const quotesDB = ["<p>&ldquo;Every Hypebeast poops.&rdquo;</p> <p>- The Weekdy</p>", "<p>&ldquo;Number 1 choice for a number 2.&rdquo;</p> <p>- Kwanye East</p>", "<p>&ldquo;What a ridiculous bunch of totally uncool jokers.&rdquo;</p> <p>- Karbara Bruger</p>"];
 let index = 0;
 
 function getNextQuote() {
-    quote.innerHTML = quotes[index].innerHTML;
-    if(index < quotes.length-1) {
+    quote.innerHTML = quotesDB[index];
+    if(index < quotesDB.length-1) {
         index++; 
     } else {
         index = 0;
